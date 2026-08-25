c8882de461f4ec32b0623f067c8cc8f358a685d8
srm11671 <seanrmulcahy@gmail.com>
2026-08-25 02:42:18 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import { NTB_TIERS } from "@/types"
 import Link from "next/link"
 
 export const dynamic = "force-dynamic"
+export const revalidate = 0
+export const fetchCache = "force-no-store"
 
 export default async function HomePage() {
   const visits = await getVisits()
