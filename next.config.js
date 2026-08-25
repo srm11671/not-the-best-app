539c6ff18ed160f1e80abad80df68d8f20f50544
srm11671 <seanrmulcahy@gmail.com>
2026-08-24 22:09:46 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-experimental: {
-  workerThreads: false,
-  cpus: 1,
-  staleTimes: {
-    dynamic: 0,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    workerThreads: false,
+    cpus: 1,
+    staleTimes: {
+      dynamic: 0,
+    },
   },
-},
+};
+
+module.exports = nextConfig;
